c49c31defae89d3a5d5b4feaa4d7c535ceb52a0d
Ashutosh <102981372+ashutosh017@users.noreply.github.com>
2022-11-19 20:43:09 +0530

    Add files via upload

---
+++ Chapter 1 Variables, Constants & Keywords/01_practice_b.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int length,breadth;
+    printf("What is the length of the ractangle\n");
+    scanf("%d",&length);
+    printf("What is the breadth of the ractangle\n");
+    scanf("%d",&breadth);
+    printf("The area of ractangle is %d \n",length*breadth);
+    return 0;
+}
---
+++ Chapter 1 Variables, Constants & Keywords/02_practice.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int radius = 3;
+    float pi = 3.14;
+    printf("The area of circle is %f \n", pi*radius*radius);
+    int height =3;
+    printf("The Volume of the cylinder is %f \n", pi*radius*radius*height);
+    return 0;
+}
